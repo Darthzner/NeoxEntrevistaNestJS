37397a654324fd8491ae1d5cc59aa32fec3690c5
Darthzner <lester.carrasco@mail.udp.cl>
2022-11-13 16:49:11 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.enableShutdownHooks();  app.useGlobalPipes(new ValidationPipe());  
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
